3adbe361b9570dd66a202f10593235bc9db00f5c
AStulova <51452277+AStulova@users.noreply.github.com>
2019-10-30 18:17:20 +0300

    Removed extra brackets

--- Java/1.1/Sort.java
+++ Java/1.1/Sort.java
@@ -1,28 +1,22 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 class Sort {
     private static void printArray(int[] arr)
     {
-        for (int value : arr) {
+        for (int value : arr)
             System.out.print(value + " ");
-        }
         System.out.println();
     }
     // Bubble sort algorithm
     private static void sortByBubble(int[] arr, int size) {
         int t;
-        for (int i = 1; i < size; i++) {
-            for (int j = size - 1; j >= i; j--) {
-                {
+        for (int i = 1; i < size; i++)
+            for (int j = size - 1; j >= i; j--)
                     if (arr[j - 1] > arr[j]) {
                         t = arr[j - 1];
                         arr[j - 1] = arr[j];
                         arr[j] = t;
                     }
-                }
-            }
-        }
         System.out.println("Sorted array by bubble method: ");
         printArray(arr);
     }
@@ -32,12 +26,11 @@ class Sort {
         for (int i = size - 1; i > 0; i--) {
             max = arr[0];
             n = 0;
-            for (int j = 1; j < i + 1; j++) {
+            for (int j = 1; j < i + 1; j++)
                 if(arr[j] > max) {
                     max = arr[j];
                     n = j;
                 }
-            }
             t = arr[i];
             arr[i] = arr[n];
             arr[n] = t;
@@ -51,14 +44,15 @@ class Sort {
         System.out.println("Sorted array by Arrays.sort():");
         printArray(arr);
     }
-
+    private static void fillArray(int[] arr){
+        for(int i = 0; i < arr.length; i++)
+            arr[i] = (int)((Math.random() * 200) - 100);
+    }
     public static void main(String[] args) {
         int[] arr, arr1, arr2;
         long startTime;
         arr = new int[10];
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = (int)((Math.random() * 200) - 100);
-        }
+        fillArray(arr);
         arr1 = arr2 = arr.clone();
         int size = arr.length;
         System.out.println("Source array: ");
