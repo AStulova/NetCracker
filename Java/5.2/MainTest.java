c2eb02a10ba08cc62f34dfe7a945db6ce7e31488
Anastasia <stacy.forever@mail.com>
2019-12-08 15:47:14 +0300

    Added some changes and conclusion

--- Java/5.2/MainTest.java
+++ Java/5.2/MainTest.java
@@ -4,7 +4,7 @@ public class MainTest {
     public static void compA() {
         long startTime, startTime1, startTime2, startTime3, startTime4;
 
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        List<Integer> arrayList = new ArrayList<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 50000; i++) {
             arrayList.add(i);
@@ -12,21 +12,21 @@ public class MainTest {
         System.out.println("arrayList.add(i) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        arrayList.add(5000,243);
-        System.out.println("arrayList.add(5000,243) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        arrayList.add(25000,243);
+        System.out.println("arrayList.add(25000,243) - execution time: " + (startTime2 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        arrayList.set(8000, 123);
-        System.out.println("arrayList.set(8000, 123) - execution time: " + (startTime3 = System.nanoTime() - startTime));
+        arrayList.set(25000, 123);
+        System.out.println("arrayList.set(25000, 123) - execution time: " + (startTime3 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        arrayList.remove(6500);
-        System.out.println("arrayList.remove(6500) - execution time: " + (startTime4 = System.nanoTime() - startTime));
+        arrayList.remove(25000);
+        System.out.println("arrayList.remove(25000) - execution time: " + (startTime4 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2 + startTime3 + startTime4));
         System.out.println();
 
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        List<Integer> linkedList = new LinkedList<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 50000; i++) {
             linkedList.add(i);
@@ -34,23 +34,23 @@ public class MainTest {
         System.out.println("linkedList.add(i) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        linkedList.add(5000,243);
-        System.out.println("linkedList.add(5000,243) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        linkedList.add(25000,243);
+        System.out.println("linkedList.add(25000,243) - execution time: " + (startTime2 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        linkedList.set(8000, 123);
-        System.out.println("linkedList.set(8000, 123) - execution time: " + (startTime3 = System.nanoTime() - startTime));
+        linkedList.set(25000, 123);
+        System.out.println("linkedList.set(25000, 123) - execution time: " + (startTime3 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        linkedList.remove(6500);
-        System.out.println("linkedList.remove(6500) - execution time: " + (startTime4 = System.nanoTime() - startTime));
+        linkedList.remove(25000);
+        System.out.println("linkedList.remove(25000) - execution time: " + (startTime4 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2 + startTime3 + startTime4));
     }
 
     public static void compB() {
         long startTime, startTime1, startTime2;
 
-        HashSet<Integer> hashSet = new HashSet<>();
+        Set<Integer> hashSet = new HashSet<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 50000; i++) {
             hashSet.add(i);
@@ -58,12 +58,12 @@ public class MainTest {
         System.out.println("hashSet.add(i) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        hashSet.remove(6500);
-        System.out.println("hashSet.remove(6500) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        hashSet.remove(25000);
+        System.out.println("hashSet.remove(25000) - execution time: " + (startTime2 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2));
         System.out.println();
 
-        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 50000; i++) {
             linkedHashSet.add(i);
@@ -71,12 +71,12 @@ public class MainTest {
         System.out.println("linkedHashSet.add(i) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        linkedHashSet.remove(6500);
-        System.out.println("linkedHashSet.remove(6500) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        linkedHashSet.remove(25000);
+        System.out.println("linkedHashSet.remove(25000) - execution time: " + (startTime2 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2));
         System.out.println();
 
-        TreeSet<Integer> treeSet = new TreeSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 50000; i++) {
             treeSet.add(i);
@@ -84,15 +84,15 @@ public class MainTest {
         System.out.println("treeSet.add(i) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        treeSet.remove(6500);
-        System.out.println("treeSet.remove(6500) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        treeSet.remove(25000);
+        System.out.println("treeSet.remove(25000) - execution time: " + (startTime2 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2));
     }
 
     public static void compC() {
         long startTime, startTime1, startTime2, startTime3;
 
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        Map<Integer, Integer> hashMap = new HashMap<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 10000; i++)
             for (int j = 0; j < 10000; j++) {
@@ -101,17 +101,17 @@ public class MainTest {
         System.out.println("hashMap.put(i, j) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        hashMap.put(8000, 123);
-        System.out.println("hashMap.put(8000, 123) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        hashMap.put(5000, 123);
+        System.out.println("hashMap.put(5000, 123) - execution time: " + (startTime2 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        hashMap.remove(6500);
-        System.out.println("hashMap.remove(6500) - execution time: " + (startTime3 = System.nanoTime() - startTime));
+        hashMap.remove(5000);
+        System.out.println("hashMap.remove(5000) - execution time: " + (startTime3 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2 + startTime3));
         System.out.println();
 
 
-        LinkedHashMap<Integer,Integer> linkedHashMap = new LinkedHashMap<>();
+        Map<Integer,Integer> linkedHashMap = new LinkedHashMap<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 10000; i++)
             for (int j = 0; j < 10000; j++) {
@@ -120,17 +120,17 @@ public class MainTest {
         System.out.println("linkedHashMap.put(i, j) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        linkedHashMap.put(8000, 123);
-        System.out.println("linkedHashMap.put(8000, 123) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        linkedHashMap.put(5000, 123);
+        System.out.println("linkedHashMap.put(5000, 123) - execution time: " + (startTime2 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        linkedHashMap.remove(6500);
-        System.out.println("linkedHashMap.remove(6500) - execution time: " + (startTime3 = System.nanoTime() - startTime));
+        linkedHashMap.remove(5000);
+        System.out.println("linkedHashMap.remove(5000) - execution time: " + (startTime3 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2 + startTime3));
         System.out.println();
 
 
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        Map<Integer, Integer> treeMap = new TreeMap<>();
         startTime = System.nanoTime();
         for (int i = 0; i < 10000; i++)
             for (int j = 0; j < 10000; j++) {
@@ -139,12 +139,12 @@ public class MainTest {
         System.out.println("treeMap.put(i, j) - execution time: " + (startTime1 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        treeMap.put(8000, 123);
-        System.out.println("treeMap.put(8000, 123) - execution time: " + (startTime2 = System.nanoTime() - startTime));
+        treeMap.put(5000, 123);
+        System.out.println("treeMap.put(5000, 123) - execution time: " + (startTime2 = System.nanoTime() - startTime));
 
         startTime = System.nanoTime();
-        treeMap.remove(6500);
-        System.out.println("treeMap.remove(6500) - execution time: " + (startTime3 = System.nanoTime() - startTime));
+        treeMap.remove(5000);
+        System.out.println("treeMap.remove(5000) - execution time: " + (startTime3 = System.nanoTime() - startTime));
         System.out.println("TOTAL EXECUTION TIME: " + (startTime1 + startTime2 + startTime3));
     }
 
